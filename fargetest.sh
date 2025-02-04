45f3f1228aa28a8107616d40073763f83f4b7618
tuvalisle <151667091+tuvalisle@users.noreply.github.com>
2025-02-04 10:51:02 +0000

    path

--- fargetest.sh
+++ fargetest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /Users/tuvalislevand/robuste/base.sh
+source /Users/tuvalislevand/robuste/dcsg2003/base.sh
 info "Dette er en infotekst"
 error "Dette er en feilmelding"
 warn "Dette er en advarsel"
